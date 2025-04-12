38778d6a1b439516dcac07c7dc2e27dd2db48d96
kumarvikash010203 <kumarvikash010203@gmail.com>
2025-04-12 09:29:48 +0530

    first commit

--- index.js
+++ index.js
@@ -25,7 +25,10 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-}); 
+});
+
+// For Deta Space
+module.exports = app; 
